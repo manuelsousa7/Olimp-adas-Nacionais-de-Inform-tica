c29e9d9cfbc244a54582d29126ecc00b91dcce0b
Manuel Sousa <manuelsousamvs@gmail.com>
2015-05-22 14:45:14 +0100

    update

--- 10 Problemas Iniciais/J100.cpp
+++ 10 Problemas Iniciais/J100.cpp
@@ -1,25 +1,27 @@
 #include <cmath>
 #include <iostream>
- 
+
+using namespace std;
+
 struct Coord{
 	double x,y;
 };
- 
-struct Coord getTileCoordinates(double tileNum) {
+
+struct Coord coordenadas(double tileNum) {
  	double doubleRoot=floor(sqrt(tileNum));
     struct Coord result;
- 	result.x =(round(doubleRoot/2)*pow(-1,doubleRoot+1))+(pow(-1,doubleRoot+1)*(((doubleRoot*(doubleRoot+1))-tileNum)-std::abs((doubleRoot*(doubleRoot+1))-tileNum))/2);
- 	result.y =(round(doubleRoot/2)*pow(-1,doubleRoot))+(pow(-1,doubleRoot+1)*(((doubleRoot*(doubleRoot+1))-tileNum)+std::abs((doubleRoot*(doubleRoot+1))-tileNum))/2);
+ 	result.x =(round(doubleRoot/2)*pow(-1,doubleRoot+1))+(pow(-1,doubleRoot+1)*(((doubleRoot*(doubleRoot+1))-tileNum)-abs((doubleRoot*(doubleRoot+1))-tileNum))/2);
+ 	result.y =(round(doubleRoot/2)*pow(-1,doubleRoot))+(pow(-1,doubleRoot+1)*(((doubleRoot*(doubleRoot+1))-tileNum)+abs((doubleRoot*(doubleRoot+1))-tileNum))/2);
     return result;
 }
- 
- 
+
+
 int main()
 {
 	int n;
-	scanf("%d",&n);
-	struct Coord result = getTileCoordinates(n-1);
-	std::cout<<'('<<result.x<<","<<result.y<<')'<<std::endl;
+	cin >> n;
+	struct Coord result = coordenadas(n-1);
+	cout << '(' << result.x << "," << result.y << ')' << endl;
 	return 0;
 }
 
